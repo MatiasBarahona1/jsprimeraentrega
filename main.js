765eb91d391978a9d6b8aa5ae3c6cfc53205147b
matiasbarahona <matydjchinomambo@gmail.com>
2023-10-11 23:54:55 -0300

    segundocommit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
 //for (let i = 1; i <= 20; i ++) {
-//    let ingresarNombre = prompt ("ingresar nombre") ;
-  //  alert (" Turno N° "+i+" nombre: "+ingresarNombre) ;
-// }
+ //let ingresarNombre = prompt ("ingresar nombre") ;
+// alert (" Turno N° "+i+" nombre: "+ingresarNombre) ;}
+
 
 let userGuardado = "matias";
 let passGuardado = "matias123";
@@ -10,7 +10,7 @@ let passGuardado = "matias123";
 const login = () => {
     let acceder = false
     for(let i = 3;i > 0 ; i--){
-        let username = prompt("Ingresa tu nombre de usuario");
+        let username = prompt("Ingrese el nombre de quien esta reservando la cancha");
         let password = prompt("Ingresa tu contraseña");
 
         if(username == userGuardado && password == passGuardado){
@@ -23,5 +23,18 @@ const login = () => {
     }
     return acceder;
 }
+login("Error! Intentelo nuevamente dentro de 15 minutos");
 
-login("Error! Intentelo nuevamente dentro de 30 minutos");
+let ingreseElDia = prompt ("Ingrese el dia que quiera reservar la cancha: LUNES, MARTES,MIERCOLES,JUEVES,VIERNES}")
+ if (ingreseElDia == lunes )
+ if (ingreseElDia == martes )
+ if (ingreseElDia == miercoles )
+ if (ingreseElDia == jueves )
+ if (ingreseElDia == viernes )
+
+let ingreseElHorario = prompt ("Ingrese el horarios deseado")
+let lunes
+let martes
+let miercoles
+let jueves 
+let viernes
